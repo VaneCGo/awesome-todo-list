a751a8e357f534a99fd80cb78e2e3c391504aab0
Vanessa Cantero Gómez <vane.cantero.g@gmail.com>
2015-03-10 19:59:10 +0100

    Delete todo list items.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,16 +1,15 @@
 class TodoItemsController < ApplicationController
+  before_action :find_todo_list
+
   def index
-  	@todo_list = TodoList.find(params[:todo_list_id])
   end
 
   # GET /todo_lists/new
   def new
-  	@todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.new
   end
 
   def create
-  	@todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.new(todo_item_params)
 
     if @todo_item.save
@@ -24,7 +23,6 @@ class TodoItemsController < ApplicationController
   end
 
   def update
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.update_attributes(todo_item_params)
       flash[:success] = 'Saved todo list item.'
@@ -36,8 +34,17 @@ class TodoItemsController < ApplicationController
 
   end
 
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = 'Todo list item was deleted.'
+    else
+      flash[:error] = 'Todo list item could not be deleted.'
+    end
+    redirect_to todo_list_todo_items_path
+  end
+
   def edit
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
@@ -46,6 +53,11 @@ class TodoItemsController < ApplicationController
   end
 
 	private
+
+  def find_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
 	def todo_item_params
 		params[:todo_item].permit(:content)
 	end
