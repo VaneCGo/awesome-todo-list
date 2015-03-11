b3179ec05055d24e3959263427c16e3fe279a09c
Vanessa Cantero Gómez <vane.cantero.g@gmail.com>
2015-03-11 15:51:41 +0100

    Show complete and incomplete items.

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -3,6 +3,10 @@ class TodoItem < ActiveRecord::Base
 
   validates :content, presence: true, length: { minimum: 2 }
 
+  # Two different ways to write the same thing
+  scope :complete, -> { where('completed_at is not null') } #where clause in the  database
+  scope :incomplete, -> { where(completed_at: nil) } #rails
+
   def completed?
   	!completed_at.blank?
   end
--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -2,4 +2,33 @@ require 'rails_helper'
 
 RSpec.describe TodoList, type: :model do
   it { should have_many(:todo_items) }
+
+  describe '#has_completed_items?' do
+  	let!(:todo_list) { TodoList.create(title: 'Groceries', description: 'Grocery list') }
+
+  	it 'returns true with completed todo list items' do
+  		todo_list.todo_items.create(content: 'My item one', completed_at: 1.minute.ago)
+  		expect(todo_list.has_completed_items?).to be_truthy
+  	end
+
+  	it 'returns false with no completed todo list items' do
+  		todo_list.todo_items.create(content: 'My item one')
+  		expect(todo_list.has_completed_items?).to be_falsey
+  	end
+  end
+
+   describe '#has_incompleted_items?' do
+  	let!(:todo_list) { TodoList.create(title: 'Groceries', description: 'Grocery list') }
+
+  	it 'returns true with incompleted todo list items' do
+  		todo_list.todo_items.create(content: 'My item one')
+  		expect(todo_list.has_incompleted_items?).to be_truthy
+  	end
+
+  	it 'returns false with no incompleted todo list items' do
+  		todo_list.todo_items.create(content: 'My item one', completed_at: 1.minute.ago)
+  		expect(todo_list.has_incompleted_items?).to be_falsey
+  	end
+  end
+
 end
